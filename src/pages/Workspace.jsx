9890754ea247cb486482510b5f84c740fdbefa67
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-03-03 19:01:05 +0900

    Chore: create react app

---
+++ src/pages/Workspace.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WorkSpace() {
+  return (
+    <div></div>
+  )
+}
+
+export default WorkSpace
---
+++ src/shared/Router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
